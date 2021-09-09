70977b589af9f070a5e2d5848de4defadde3d6b8
tom_glb <tom_glb@hncj.cn>
2021-09-09 13:58:38 +0800

    init rep

---
+++ src/test/java/cn/hncj/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hncj.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
